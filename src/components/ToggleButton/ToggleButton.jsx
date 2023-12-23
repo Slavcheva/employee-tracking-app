46a4ce393a9a8af05a7ae8e5044d47301e81867e
Slavcheva <stoyka.slavcheva@gmail.com>
2023-12-23 13:37:28 +0200

    Update toggle button styles

--- src/components/ToggleButton/ToggleButton.jsx
+++ src/components/ToggleButton/ToggleButton.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
+import "./ToggleButton.css"
 import up from "../../assets/angle-up.png";
 import down from "../../assets/angle-down.png";
 
-function ToggleButton({id, elementId, open, toggleTable, msg}) {
+function ToggleButton({id, elementId, open, toggleTable, msg = 'All common projects for these employees'}) {
 
     return (
         <div className="top item-field-id">
-            {msg
-                ? <div>{msg}</div>
-                : <div>All common projects for these employees</div>
-            }
-            <div className="project-btns">
-                <div className="project-btn arrow-btn" onClick={toggleTable}
+            <div>{msg}</div>
+            <div className="toggle-btn">
+                <div className="arrow-btns" onClick={toggleTable}
                      id={elementId}>
                     {(open && id === elementId) ? (
                         <img src={up} alt="up-arrow"/>
