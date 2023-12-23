839b7d713cb05064d7953a25a16c1bf55a1fecd1
Slavcheva <stoyka.slavcheva@gmail.com>
2023-12-23 14:41:44 +0200

    Add PairsComponent

---
+++ src/components/PairsComponent/PairsComponents.jsx
@@ -0,0 +1,66 @@
+import React, {useContext, useState} from 'react';
+import {FileContext} from "../../contexts/FileContext";
+import Table from "../Table/Table";
+import ToggleButton from "../ToggleButton/ToggleButton";
+import SubMenu from "../SubMenu/SubMenu";
+import TopPairMessages from "../TopPairMessages/TopPairMessages";
+import Search from "../Search/Search";
+
+function PairsComponents({data, empHeadings, projHeadings, pageTop = false}) {
+
+    const [open, setOpen] = useState(false);
+    const [id, setId] = useState(null);
+    const [search, setSearch] = useState('')
+
+    const {file} = useContext(FileContext);
+
+
+    function toggleTable(e) {
+        e.currentTarget.id ? setId(e.currentTarget.id) : setId(null)
+        setOpen(!open)
+    }
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setSearch(e.target.value);
+    };
+
+
+    return (<div className="sub-section">
+            <SubMenu/>
+            {!!file ?
+                <>
+                    {/*<div className="sub-section-bar">*/}
+                    {pageTop&&<TopPairMessages data={data}/>}
+                    {/*</div>*/}
+
+                    <div className="search-pairs">
+                        <Search handleChange={handleChange}
+                                placeholder="Search by employees ids"/>
+                    </div>
+
+                    {data.filter((row) => {
+                        return search === ''
+                            ? row
+                            : row.pair.join(" ").includes(search)
+                    }).map((row, index) => {
+                        return (
+                            <div key={row.pair.join(" ")} className="item-field">
+                                <Table data={[[row.pair.join(", "), row.total.toString()]]} headings={empHeadings}
+                                       id={row.pair.join(", ")}/>
+
+                                <ToggleButton id={id} elementId={row.pair.join(", ")} toggleTable={toggleTable}
+                                              open={open}/>
+                                {(open && id === row.pair.join(", ")) && (
+                                    <Table data={row.projectsObj.map(pr => [pr.project, pr.days])}
+                                           headings={projHeadings} id={row.pair.join(", ") + "*" + index}/>
+                                )}
+                            </div>)
+                    })}
+                </>
+                : <div>Please upload your csv file to see the data!</div>}
+        </div>
+    );
+}
+
+export default PairsComponents;
